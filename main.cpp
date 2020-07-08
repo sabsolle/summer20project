80e40999ca8e64101960b7fdecb555aee341c3ea
Matt Schrader <mattschrader2@gmail.com>
2020-07-08 15:18:19 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,15 +7,25 @@ int main()
 {	
 	string name = " ";
 
+
+
 	cout << "What is your name?" << endl;
 	cin >> name;
-	cout << "What is your date of birth?" << endl;
+	cout << "Which category are you in?" << endl;
+	cout << "a) March 21 - April 21" << endl;
+	cout << "b) April 20 - May 20" << endl;
+	cout << "c) May 21 - June 20" << endl;
+	cout << "d) June 21 - July 22" << endl;
+	cout << "e) July 23 - August 22" << endl;
+	cout << "f) August 23 - September 23" << endl;
+	cout << "g) September 23 - October 22" << endl;
+	cout << "h) October 23 - November 21" << endl;
+	cout << "i) November 22 - December 21" << endl;
+	cout << "j) December 22 - January 19" << endl;
+	cout << "k) January 20 - February 18" << endl;
+	cout << "l) February 19 - March 20" << endl;
 
-	cout << ":)" << endl;
-
-	cout << "confused" << endl;
-
-	cout << "Penis" << endl;
+	
 
 
 
